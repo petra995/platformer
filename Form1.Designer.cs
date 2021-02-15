141adb790db92c256ad49df8b148d1c24ee50e6e
Oleg Petrov <mr.petroll@mail.ru>
2021-02-15 22:17:22 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace platformer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(11, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 184);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(442, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 501);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace platformer
+{
+    public partial class Form1 : Form
+    {
+        
+        Image playerImg;
+        private int currFrame = 0;
+        private int currAnimation = 0;
+        bool isAnyKeyPressed = false;
+        int speed = 2;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+            playerImg = new Bitmap("C:\\adv_chara1.png");
+            timer2.Interval = 10;
+            timer2.Tick += new EventHandler(updMove);
+            timer2.Start();
+            timer1.Tick += new EventHandler(update);
+            timer1.Interval = 100;
+            timer1.Start();
+            this.KeyDown += new KeyEventHandler(keyboard);
+            this.KeyUp += new KeyEventHandler(free_keyboard);
+
+        }
+
+        private void updMove(object sender, EventArgs e)
+        {
+            switch (currAnimation)
+            {
+                case 0:
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + speed, pictureBox1.Location.Y);
+                    break;
+                case 1:
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - speed, pictureBox1.Location.Y);
+                    break;
+                case 3:
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - speed);
+                    break;
+                case 2:
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + speed);
+                    break;
+            }
+        }
+
+        private void free_keyboard(object sender, KeyEventArgs e)
+        {
+            isAnyKeyPressed = false;
+            switch (e.KeyCode.ToString())
+            {
+                case "D":
+                    currAnimation = 5;
+                    break;
+                case "A":
+                    currAnimation = 6;
+                    break;
+                case "W":
+                    currAnimation = 8;
+                    break;
+                case "S":
+                    currAnimation = 7;
+                    break;
+            }
+            currFrame = 0;
+            label1.Text = "LastKey: " + e.KeyCode.ToString();
+        }
+
+        private void keyboard(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode.ToString())
+            {
+                case "D":
+                    currAnimation = 0;
+                    //pictureBox1.Location = new Point(pictureBox1.Location.X + speed, pictureBox1.Location.Y);
+                    break;
+                case "A":
+                    currAnimation = 1;
+                   // pictureBox1.Location = new Point(pictureBox1.Location.X - speed, pictureBox1.Location.Y);
+                    break;
+                case "W":
+                    currAnimation = 3;
+                    //pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - speed);
+                    break;
+                case "S":
+                    currAnimation = 2;
+                   // pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + speed);
+                    break;
+            }
+            
+            isAnyKeyPressed = true;
+        }
+
+        private void update(object sender, EventArgs e)
+        {
+            if (isAnyKeyPressed)
+            {
+                timer1.Interval = 100;
+                playAnimationMove();
+                if (currFrame == 11)
+                {
+                    currFrame = 2;
+                }
+                
+            }
+            else
+            {
+                timer1.Interval = 100;
+                playAnimationIdle();
+                if (currFrame == 2)
+                {
+                    currFrame = 0;
+                }
+                
+
+            }
+            currFrame++;
+        }
+        private void playAnimationIdle()
+        {
+            if (currAnimation >= 5)
+            {
+                Image part = new Bitmap(100, 230);
+                Graphics g = Graphics.FromImage(part);
+                g.DrawImage(playerImg, 0, 0, new Rectangle(new Point(75 * currFrame, 170 * (currAnimation-5)), new Size(100, 230)), GraphicsUnit.Pixel);
+                pictureBox1.Size = new Size(100, 230);
+                pictureBox1.Image = part;
+            }
+
+        }
+        private void playAnimationMove()
+        {
+            if(currAnimation != -1 && currAnimation <= 4)
+            {
+                Image part = new Bitmap(100, 230);
+                Graphics g = Graphics.FromImage(part);
+                g.DrawImage(playerImg, 0, 0, new Rectangle(new Point(75 * currFrame, 170 * currAnimation), new Size(100, 230)), GraphicsUnit.Pixel);
+                pictureBox1.Size = new Size(100, 230);
+                pictureBox1.Image = part;
+            }
+            
+        }
+    }
+}
